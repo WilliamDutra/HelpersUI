b2feadf4797302bbf17fa81303c6356d1a31eeaf
William Dutra <will.dutra26@gmail.com>
2022-04-26 10:47:40 -0300

    Criação do projeto

---
+++ HelpersUI.WPF/FRMS/FrmFiltro.xaml.cs
@@ -0,0 +1,27 @@
+﻿using HelpersUI.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelpersUI.WPF.FRMS
+{
+    /// <summary>
+    /// Lógica interna para FrmFiltro.xaml
+    /// </summary>
+    public partial class FrmFiltro : Window
+    {
+        public FrmFiltro()
+        {
+            InitializeComponent();
+            DataContext = new FrmFiltroViewModel();
+        }
+    }
+}
---
+++ HelpersUI.WPF/ViewModels/FrmFiltroViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace HelpersUI.WPF.ViewModels
+{
+    public class FrmFiltroViewModel : VMBase
+    {
+        public FrmFiltroViewModel()
+        {
+            ListBoxItemSource = new ObservableCollection<ListBoxItemSource>();
+            ListBoxItemSource.Add(new ViewModels.ListBoxItemSource
+            {
+                Nome = "Corinthians",
+                IsChecked = false
+            });
+            ListBoxItemSource.Add(new ViewModels.ListBoxItemSource
+            {
+                Nome = "São Paulo",
+                IsChecked = false
+            });
+            ListBoxItemSource.Add(new ViewModels.ListBoxItemSource
+            {
+                Nome = "Palmeiras",
+                IsChecked = false
+            });
+        }
+
+        private ObservableCollection<ListBoxItemSource> _ListBoxItemSource;
+
+        public ObservableCollection<ListBoxItemSource> ListBoxItemSource
+        {
+            get { return _ListBoxItemSource; }
+            set { SetProperty(ref _ListBoxItemSource, value); }
+        }
+
+    }
+
+    public class ListBoxItemSource
+    {
+        public string Nome { get; set; }
+
+        public bool IsChecked { get; set; } = false;
+
+    }
+}
