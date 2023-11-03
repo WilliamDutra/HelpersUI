d248a4681ce4d718c3fc926c4e19ef42eca7ce36
William Dutra <will.dutra26@gmail.com>
2023-11-03 20:27:33 -0300

    funcionalidade: inclusão da leitura do excel

---
+++ Src/HelpersUI/Excel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using MiniExcelLibs;
+using HelpersUI.Interfaces;
+using System.Collections.Generic;
+
+namespace HelpersUI
+{
+    public class Excel : IExcel
+    {
+        public IEnumerable<T> Ler<T>(string arquivo) where T : class, new()
+        {
+            var stream = new FileStream(path: arquivo, mode: FileMode.Open);
+            return stream.Query<T>();
+        }
+
+        public IEnumerable<T> Ler<T>(Stream arquivo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/HelpersUI/Interfaces/IExcel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace HelpersUI.Interfaces
+{
+    public interface IExcel
+    {
+        IEnumerable<T> Ler<T>(string arquivo) where T : class, new();
+
+        IEnumerable<T> Ler<T>(Stream arquivo);
+
+    }
+}
